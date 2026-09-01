233185d17dd22c9ece331d360fdad57a0d3efbac
maurojh <maurojh@gmail.com>
2026-09-01 11:22:22 -0300

    Add files via upload

---
+++ botao_resposta.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BotaoReposta extends StatelessWidget {
+  BotaoReposta({
+    super.key,
+    required this.textoResposta,
+    required this.callResposta, // nomeada
+    required this.cor,
+  });
+
+  final String textoResposta;
+  final void Function() callResposta;
+  final Color cor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: cor,
+        foregroundColor: Colors.white, // texto
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            15.0,
+          ),
+        ),
+        padding: const EdgeInsets.all(20.0),
+      ),
+      onPressed: callResposta, // será chamada no click
+      child: Text(textoResposta),
+    );
+  }
+}
